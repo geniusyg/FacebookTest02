ddca473ce63d38e3d8697de5995d3ee2dc1c9cdc
Chae Yungi <l8673901@gmail.com>
2014-01-21 22:12:38 +0900

    Initial Commit

---
+++ FacebookTest02Tests/FacebookTest02Tests.m
@@ -0,0 +1,34 @@
+//
+//  FacebookTest02Tests.m
+//  FacebookTest02Tests
+//
+//  Created by yg on 2014. 1. 21..
+//  Copyright (c) 2014년 yg. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FacebookTest02Tests : XCTestCase
+
+@end
+
+@implementation FacebookTest02Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
