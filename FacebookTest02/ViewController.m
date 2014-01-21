f152042aa7e9ccde2c4ee3126f6f5bfd970240b4
Chae Yungi <l8673901@gmail.com>
2014-01-21 22:30:31 +0900

    FacebookTest02
    페북 친구목록

--- FacebookTest02/ViewController.m
+++ FacebookTest02/ViewController.m
@@ -7,12 +7,86 @@
 //
 
 #import "ViewController.h"
+#import <Accounts/Accounts.h>
+#import <Social/Social.h>
 
-@interface ViewController ()
+#define FACEBOOK_APPID @"234507400054801"
+
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *table;
+
+@property ACAccount *fa;
+@property NSArray *data;
 
 @end
 
 @implementation ViewController
+
+- (void)showFriendsList {
+    ACAccountStore *store = [[ACAccountStore alloc] init];
+    ACAccountType *accountType = [store accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierFacebook];
+    NSDictionary *options = @{ACFacebookAppIdKey: FACEBOOK_APPID,
+                              ACFacebookPermissionsKey: @[@"user_friends"],
+                              ACFacebookAudienceKey: ACFacebookAudienceEveryone};
+    [store requestAccessToAccountsWithType:accountType options:options completion:^(BOOL granted, NSError *error) {
+        if (error) {
+            NSLog(@"Error : %@", error);
+        }
+        if (granted) {
+            NSLog(@"권한 승인 성공");
+            NSArray *accountList = [store accountsWithAccountType:accountType];
+            self.fa = [accountList lastObject];
+            
+            [self requestFriends];
+        }else {
+            NSLog(@"권한 승인 실패");
+        }
+    }];
+}
+
+- (void)requestFriends {
+    NSString *urlStr = @"https://graph.facebook.com/me/friends";
+    NSURL *url = [NSURL URLWithString:urlStr];
+    NSDictionary *params = nil;
+    SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeFacebook requestMethod:SLRequestMethodGET URL:url parameters:params];
+    request.account = self.fa;
+    
+    [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+        if (nil != error) {
+            NSLog(@"Error : %@", error);
+            return;
+        }
+        __autoreleasing NSError *parseError;
+        NSDictionary *result = [NSJSONSerialization JSONObjectWithData:responseData options:kNilOptions error:&parseError];
+        
+        self.data = result[@"data"];
+
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            [self.table reloadData];
+        }];
+    }];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.data count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FRIENDS_CELL"];
+    
+    NSDictionary *one = self.data[indexPath.row];
+    
+    NSString *contents;
+	
+    contents = one[@"name"];
+    cell.textLabel.text = contents;
+    return cell;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [self showFriendsList];
+}
+
 
 - (void)viewDidLoad
 {
@@ -27,3 +101,36 @@
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
